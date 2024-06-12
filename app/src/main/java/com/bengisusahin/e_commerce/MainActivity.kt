d7b5f83ec56f8dbf67ca42787396c9237070758d
Bengisu Şahin <74653216+bengisusaahin@users.noreply.github.com>
2024-06-12 18:54:18 +0300

    feat: added drawer navigation

--- app/src/main/java/com/bengisusahin/e_commerce/MainActivity.kt
+++ app/src/main/java/com/bengisusahin/e_commerce/MainActivity.kt
@@ -2,16 +2,69 @@ package com.bengisusahin.e_commerce
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
+import com.bengisusahin.e_commerce.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val toolbar = binding.toolbar
+        setSupportActionBar(toolbar)
+        /*
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        val navController = navHostFragment.navController
+         */
+        val navController = findNavController(R.id.fragmentContainerView)
+        val drawerLayout = binding.drawerLayout
+        val navView = binding.navigationView
+        // Set up the ActionBarDrawerToggle
+        val toggle = ActionBarDrawerToggle(
+            this, drawerLayout, toolbar,
+            R.string.navigation_drawer_open, R.string.navigation_drawer_close
+        )
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+        // Set up the action bar for use with the NavController
+        appBarConfiguration = AppBarConfiguration(navController.graph, drawerLayout)
+
+        navView.setupWithNavController(navController)
+
+        navView.setNavigationItemSelectedListener { menuItem ->
+            menuItem.isChecked = true
+            drawerLayout.closeDrawers()
+            when (menuItem.itemId) {
+                R.id.homeFragment -> {
+                    navController.navigate(R.id.homeFragment)
+                }
+                R.id.favoritesFragment -> {
+                    navController.navigate(R.id.favoritesFragment)
+                }
+                R.id.cartFragment -> {
+                    navController.navigate(R.id.cartFragment)
+                }
+                R.id.loginFragment -> {
+                    navController.navigate(R.id.loginFragment)
+                }
+            }
+            true
+        }
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.fragmentContainerView)
+        return NavigationUI.navigateUp(navController, appBarConfiguration) || super.onSupportNavigateUp()
     }
 }
